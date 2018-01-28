175bb8103b1b649f1388fa1adbcaa9ea384b5dab
William Newman <william.newman94@gmail.com>
2018-01-28 18:13:00 -0500

    mapper.py now uses csv

--- mapper.py
+++ mapper.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
 
+import csv
 import sys
 import string
 
-with open(sys.argv[1],'rb') as air:
-    lines=air.readlines()
-    lines.pop(0)
-    for l in lines:
-        d = l.replace('"','')
-        d = d.split(',')
-            
-        print d[0]
+def mapper(file_name):
+    with open(file_name) as f:
+        reader = csv.reader(f,delimiter = ',')
+        reader.next() #skip header
+        for line in reader:
+            sys.stdout.write(line[21] + ',1' + '\n')  #Zone and amount of pickups  which will always be 1
+            #print line[21] + ', 1'
+
+
+def main():
+    mapper(sys.argv[1])
+    
+if __name__ == '__main__':
+    main()
+
--- mapper3.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import sys
-import string
-
-for line in sys.stdin:   
-    d = line.replace('"','')
-    d = d.split(',')
-    print d[0]
