85fd9286ada53bbc12277c37693f3a4f5f78436d
William Newman <william.newman94@gmail.com>
2018-01-30 22:49:11 -0500

    Commented all scripts

--- mapper.py
+++ mapper.py
@@ -5,12 +5,21 @@ import sys
 import string
 
 def mapper():
+    """
+        Reads in csv from system,
+    enters it as an argument
+    in the csv.reader() function
+    and parses it. Eventually it outputs
+    the [zone, date]. Outputs to 
+    bash command sort.
+    """
+
     data = sys.stdin.readlines()
     reader = csv.reader(data,delimiter = ',')
-    reader.next()
+    reader.next() #skips header, for some reason header is not already skipped
     for line in reader:
-        if int(line[21]) > 100 and int(line[21]) <= 200:
-                sys.stdout.write(line[21] + ', ' + line[3] +  '\n')  #Zone and amount of pickups  which will always be 1
+        if int(line[21]) > 100 and int(line[21]) <= 200: # if zone is between 100 and 200
+                sys.stdout.write(line[21] + ', ' + line[3] +  '\n')  #Zone and date, counts as one pickup
 
 
 def main():
--- reducer.py
+++ reducer.py
@@ -3,58 +3,91 @@
 import csv
 import sys
 
-def add_to_list(l,iterator,date,is_new,zone):
-    if not is_new:
-        for y in l[iterator][1]:
-            if y[0] == date.strip():
-                y[1] = y[1] + 1
+def add_to_list(l,it,date,is_new,zone):
+    """
+        If new zone has not been reached by the it
+    then the function adds one to the cumulative total 
+    of the corresponding date in the list. 
+    
+        If new zone had been reached then initializes
+    a list that will eventually be outputted, appends it to
+    list l, and add one to the cumulative total of the 
+    corresponding date in the list
+    """
+    if not is_new: 
+        for y in l[it][1]: #iterates through dates [135,[<<<<"2015-12-3">>>>,1]] indicated by pointed brackets
+            print y
+            print y[0]
+            if str(y[0]) == date.strip():
+                y[1] = y[1] + 1 #adds one to corresponding date's cumulative total
                 return l
     else:
-        new = initialize_new(zone)
+        new = initialize_new(zone) #initializes new list because zone is not in l
         l.append(new)
-        for y in l[iterator][1]:
+        for y in l[it][1]:
             if str(y[0]) == date.strip():
                 y[1] = y[1] + 1
                 return l
     return l
 
 def initialize_new(zone):
-    new = [zone,[["2015-12-1",0]]]
+    """
+        Creates list consisting of 
+    the zone number and the date and 
+    initialized, cumulative pickup total.
+    """
+        
+    new = [zone,[["2015-12-1",0]]] #Begins at "2015-12-1" 
     for i in range(2,11):
-        mem = ["2015-12-" + str(i),0]
+        mem = ["2015-12-" + str(i),0] #Creates nine more consecutive members
         new[1].append(mem)
     return new
 
 def most_pickups(l):
+    """
+        Finds the date on which the most pickups
+    happened and returns that date and it's 
+    cumulative pickups
+    """
     top_date = ""
     max_pickups = -1
     for y in l:
         if y[1] > max_pickups:
-            top_date = y[0]
-            max_pickups = y[1]
-    return [top_date,max_pickups]
+            top_date = y[0] #sets top date
+            max_pickups = y[1] #sets max pickups
+    return [top_date,max_pickups] #Returns list
 
 def reduce(f):
+    """
+        Parse input and inputs each row into 
+    add_to_list function to be processed. 
+    Eventually creates list with [zone,[date,max_pickups] 
+    format. Eventually outputs newly created list
+    """
     l = []
     csv_reader = csv.reader(f)
-    iterator = -1
+    it = -1
     curr_zone = -1
     for line in csv_reader:
-        if curr_zone == int(line[0]):
-            l = add_to_list(l,iterator,line[1],False,curr_zone)
+        new_zone = int(line[0])
+        if curr_zone == new_zone: #checks zone against 
+            l = add_to_list(l,it,line[1],False,curr_zone) #False indicates not new zone
         else:
-            curr_zone = int(line[0])
-            iterator = iterator + 1
-            l = add_to_list(l,iterator,line[1],True,curr_zone)
+            curr_zone = new_zone
+            it = it + 1
+            l = add_to_list(l,it,line[1],True,curr_zone)
 
     for x in l:
-        new = most_pickups(x[1])
+        new = most_pickups(x[1]) #Finds max_pickups
         x[1] = new
     for x in l:
         x = str(x)
-        x = x.replace('[','')
+        x = x.replace('[','') #Cleans output
         x = x.replace(']','')
+        x = x.replace('\'','')
         print x
+
+
 def main():
     reduce(sys.stdin)
 
